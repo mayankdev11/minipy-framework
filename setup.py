c0a2ce3adea380f5afdf566f2117b4e20fd7b90b
mayankdev11 <mayankcool2025@gmail.com>
2026-02-28 14:33:38 +0530

    Add setup.py for minipy-framework package
    Initial setup configuration for minipy-framework package.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="minipy-framework",
+    version="0.1.0",
+    author="Your Name",
+    description="A simple Python web framework",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
